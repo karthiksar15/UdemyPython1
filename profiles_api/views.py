0acc94087df008ea4a0648fa964d2d45455e0426
karthik <karthiksar15@gmail.com>
2021-01-16 19:53:13 +0530

    djsngo get

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloApiView(APIView):
+
+    def get(self,request,format=None):
+
+        an_apiview=['Uses http methods as function (get,post,put,delete)',
+        'is smilar to django view',
+        'gives you most control',
+        'is manually mapped to urls']
+
+        return Response({'msg':'Hello!','an_apiview':an_apiview})
